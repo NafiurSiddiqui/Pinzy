78eac59c4095c483b22a98e71fb95b035df50104
Nafi R <saejim91@gmail.com>
2023-04-29 06:44:28 +0600

    [0]

--- webpack.config.js
+++ webpack.config.js
@@ -5,4 +5,8 @@ module.exports = {
   entry: {
     bundle: path.resolve(__dirname, 'src/index.js'),
   },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].js',
+  },
 };
