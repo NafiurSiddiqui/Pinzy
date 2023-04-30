d8d7407cf6becbdd6ae23be5ae3825a125d20106
Nafi R <saejim91@gmail.com>
2023-05-01 05:55:01 +0600

    guest profile cards added

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, '/index.html'),
-        nested: resolve(__dirname, 'src/html/user.html'),
+        nested: resolve(__dirname, 'src/html/pins.html'),
       },
     },
     minify: true,
