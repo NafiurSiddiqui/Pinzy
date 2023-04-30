3de0680484ae506c113f66af35d837c7bb64992e
Nafi R <saejim91@gmail.com>
2023-04-30 17:47:08 +0600

    form btn added

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,15 @@
 module.exports = {
   content: ['./index.html', './src/**/*.{html,js}', './src/*.{html,js}'],
   theme: {
+    screens: {
+      'android-md': '400px',
+      'android-md/2': '450px',
+      tablet: '640px',
+      'tablet-md': '768px',
+      laptop: '1024px',
+      desktop: '1280px',
+      'desktop-md': '1536px',
+    },
     extend: {},
   },
   plugins: [],
