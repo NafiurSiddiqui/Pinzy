f894501226882db69a68569f16fb31023bfbcf7e
Nafi R <saejim91@gmail.com>
2023-06-29 05:00:48 +0600

    updated global pins sidebar

--- src/api/pins.php
+++ src/api/pins.php
@@ -25,12 +25,12 @@ include './layout/header-pin.php';
 
 
 <!-- nav -->
-<nav class="py-4 px-4 drop-shadow-md shadow-zinc-100 bg-white">
+<nav class="py-4 px-4 drop-shadow-md shadow-zinc-100 bg-zinc-500">
   <ul class="flex justify-around">
-    <li class="border-r border-r-1 border-r-gray-300 w-4/5 text-center text-base nav-active">
+    <li class="border-r border-r-1 border-r-zinc-400 w-4/5 text-center text-base nav-active">
       Pins
     </li>
-    <li class="font-semibold w-4/5 text-center text-base text-zinc-400 hover:text-zinc-500  ">
+    <li class="font-semibold w-4/5 text-center text-base text-zinc-400 hover:text-zinc-100  ">
       <a href="
               <?php
                 echo $userLogged? "./user.php": "./guest.php";?>
@@ -40,11 +40,11 @@ include './layout/header-pin.php';
 </nav>
 
 <!-- pins -->
-<div class="pin-container-wrapper__global bg-zinc-100 h-90vh">
+<div class="pin-container-wrapper__global bg-zinc-400 h-90vh">
   <p class="default-msg text-center text-zinc-400 font-semibold text-lg top-8 relative italic">
     No pins created yet.
   </p>
-  <ul class="global-pin-container hidden px-4 pt-8 pb-4 flex items-center flex-col bg-zinc-200 h-76vh overflow-y-scroll"
+  <ul class="global-pin-container hidden px-4 pt-8 pb-4 flex items-center flex-col bg-zinc-600 h-76vh overflow-y-scroll"
     data-userLogged="
     <?php
       echo $userLogged ? 'true':'false';
