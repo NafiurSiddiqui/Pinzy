23eebbf5c8959fd56ed90f55cb4db3f8f4a1eff4
Nafi R <saejim91@gmail.com>
2023-06-28 18:43:08 +0600

    submission hover effect applied

--- src/api/signup-form.php
+++ src/api/signup-form.php
@@ -128,7 +128,7 @@ include './layout/header-auth.php';
 ?>
         </span>
     </div>
-    <button class="btn-user-input-login relative -bottom-4 w-full mt-10 mb-3 h-10 rounded font-semibold text-m text-zinc-50  android-md/2:w-52 android-md:rounded-2xl  border-4 border-green-400  bg-green-400 
+    <button class="btn-user-input-login relative -bottom-4 w-full mt-10 mb-3 h-10 rounded font-semibold text-m text-zinc-50  android-md/2:w-52 android-md:rounded-2xl  border-4 border-green-400  bg-green-400   laptop:hover:bg-green-500 laptop:hover:border-green-500
         laptop:h-full laptop:py-2 laptop:px-4 
         transition-colors active:text-zinc-100 " type="submit" name="submit">
         Signup
